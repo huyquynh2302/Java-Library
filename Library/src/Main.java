c535cb82de117ea55c7eca3a4c7b326242303d71
Huy Quynh <huyquynh11to@gmail.com>
2021-05-21 11:26:05 +0700

    Update

--- Library/src/Main.java
+++ Library/src/Main.java
@@ -16,13 +16,21 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
+import java.awt.Panel;
 
 public class Main extends JFrame {
 	
-	private Image img_logo = new ImageIcon(Main.class.getResource("icon/logo.png")).getImage().getScaledInstance(24,24,Image.SCALE_SMOOTH);
-
+	private Image img_logo = new ImageIcon(Main.class.getResource("icon/books.png")).getImage().getScaledInstance(24,24,Image.SCALE_SMOOTH);
+	private Image img_book = new ImageIcon(Main.class.getResource("icon/book.png")).getImage().getScaledInstance(24,24,Image.SCALE_SMOOTH);
+	private Image img_course = new ImageIcon(Main.class.getResource("icon/course.png")).getImage().getScaledInstance(24,24,Image.SCALE_SMOOTH);
+	private Image img_user_account = new ImageIcon(Main.class.getResource("icon/account.png")).getImage().getScaledInstance(24,24,Image.SCALE_SMOOTH);
+	private Image img_borrow_book = new ImageIcon(Main.class.getResource("icon/borrow-book.png")).getImage().getScaledInstance(24,24,Image.SCALE_SMOOTH);
+	private Image img_change_user = new ImageIcon(Main.class.getResource("icon/change-user.png")).getImage().getScaledInstance(24,24,Image.SCALE_SMOOTH);
+	private Image img_add_user = new ImageIcon(Main.class.getResource("icon/add-user.png")).getImage().getScaledInstance(24,24,Image.SCALE_SMOOTH);
+	
 	private JPanel contentPane;
-	private JTextField txtAdmin;
+	private JTextField textField;
+	private JPanel children1;
 
 	/**
 	 * Launch the application.
@@ -47,7 +55,7 @@ public class Main extends JFrame {
 		setUndecorated(true);
 		setTitle("Trang ch\u1EE7");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 828, 500);
+		setBounds(100, 100, 918, 560);
 		contentPane = new JPanel();
 		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
 		setContentPane(contentPane);
@@ -57,33 +65,38 @@ public class Main extends JFrame {
 		lblNewLabel.setBounds(40, 10, 67, 22);
 		contentPane.add(lblNewLabel);
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel.setBounds(0, 38, 828, 462);
-		contentPane.add(panel);
-		panel.setLayout(null);
+		JPanel panelMain = new JPanel();
+		panelMain.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panelMain.setBounds(200, 38, 718, 521);
+		contentPane.add(panelMain);
+		panelMain.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblIonLogo = new JLabel("");
+		lblIonLogo.setBounds(10, 5, 32, 32);
+		contentPane.add(lblIonLogo);
+		lblIonLogo.setIcon(new ImageIcon(img_logo));
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(61, 157, 245));
-		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_1.setBounds(0, 0, 200, 462);
-		panel.add(panel_1);
 		panel_1.setLayout(null);
+		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_1.setBackground(new Color(61, 157, 245));
+		panel_1.setBounds(0, 38, 200, 521);
+		contentPane.add(panel_1);
 		
 		JPanel panel_2 = new JPanel();
+		panel_2.setLayout(null);
 		panel_2.setBackground(new Color(61, 157, 245));
 		panel_2.setBounds(10, 10, 180, 49);
 		panel_1.add(panel_2);
-		panel_2.setLayout(null);
 		
-		txtAdmin = new JTextField();
-		txtAdmin.setBorder(null);
-		txtAdmin.setBackground(new Color(61, 157, 245));
-		txtAdmin.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		txtAdmin.setText("Admin");
-		txtAdmin.setBounds(52, 10, 96, 29);
-		panel_2.add(txtAdmin);
-		txtAdmin.setColumns(10);
+		textField = new JTextField();
+		textField.setText("Admin");
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		textField.setColumns(10);
+		textField.setBorder(null);
+		textField.setBackground(new Color(61, 157, 245));
+		textField.setBounds(52, 10, 96, 29);
+		panel_2.add(textField);
 		
 		JPanel panel_3_1_1_1_1_1 = new JPanel();
 		panel_3_1_1_1_1_1.setLayout(null);
@@ -94,12 +107,16 @@ public class Main extends JFrame {
 		JButton btnNewButton = new JButton("\u0110\u0103ng k\u00ED");
 		btnNewButton.setHorizontalAlignment(SwingConstants.LEFT);
 		btnNewButton.setForeground(Color.WHITE);
-		btnNewButton.setBorder(null);
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton.setBorder(null);
 		btnNewButton.setBackground(new Color(61, 157, 245));
-		
 		btnNewButton.setBounds(51, 0, 129, 49);
 		panel_3_1_1_1_1_1.add(btnNewButton);
+		
+		JLabel lblDK = new JLabel("");
+		lblDK.setBounds(10, 10, 32, 32);
+		panel_3_1_1_1_1_1.add(lblDK);
+		lblDK.setIcon(new ImageIcon(img_add_user));
 		
 		JPanel panel_3_1_1_1_1_1_1 = new JPanel();
 		panel_3_1_1_1_1_1_1.setLayout(null);
@@ -116,6 +133,11 @@ public class Main extends JFrame {
 		btnQunLTi.setBounds(51, 0, 129, 49);
 		panel_3_1_1_1_1_1_1.add(btnQunLTi);
 		
+		JLabel lblQLTK = new JLabel("");
+		lblQLTK.setBounds(9, 10, 32, 32);
+		panel_3_1_1_1_1_1_1.add(lblQLTK);
+		lblQLTK.setIcon(new ImageIcon(img_change_user));
+		
 		JPanel panel_3_1_1_1_1_1_1_1 = new JPanel();
 		panel_3_1_1_1_1_1_1_1.setLayout(null);
 		panel_3_1_1_1_1_1_1_1.setBackground(new Color(61, 157, 245));
@@ -123,13 +145,26 @@ public class Main extends JFrame {
 		panel_1.add(panel_3_1_1_1_1_1_1_1);
 		
 		JButton btnNewButton_1_1 = new JButton("Qu\u1EA3n l\u00FD m\u01B0\u1EE3n tr\u1EA3");
-		btnNewButton_1_1.setForeground(Color.WHITE);
+		btnNewButton_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				children1 = new QLMT();
+				panelMain.removeAll();
+				panelMain.add(children1);
+				panelMain.validate();
+			}
+		});
 		btnNewButton_1_1.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton_1_1.setForeground(Color.WHITE);
 		btnNewButton_1_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnNewButton_1_1.setBorder(null);
 		btnNewButton_1_1.setBackground(new Color(61, 157, 245));
 		btnNewButton_1_1.setBounds(51, 0, 129, 49);
 		panel_3_1_1_1_1_1_1_1.add(btnNewButton_1_1);
+		
+		JLabel lblQLMuonTra = new JLabel("");
+		lblQLMuonTra.setBounds(9, 10, 32, 32);
+		panel_3_1_1_1_1_1_1_1.add(lblQLMuonTra);
+		lblQLMuonTra.setIcon(new ImageIcon(img_borrow_book));
 		
 		JPanel panel_3_1_1_1_1_1_1_1_1 = new JPanel();
 		panel_3_1_1_1_1_1_1_1_1.setLayout(null);
@@ -138,13 +173,26 @@ public class Main extends JFrame {
 		panel_1.add(panel_3_1_1_1_1_1_1_1_1);
 		
 		JButton btnNewButton_1_1_1 = new JButton("Qu\u1EA3n l\u00FD \u0111\u1ED9c gi\u1EA3");
-		btnNewButton_1_1_1.setForeground(Color.WHITE);
+		btnNewButton_1_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				children1 = new QLDG1();
+				panelMain.removeAll();
+				panelMain.add(children1);
+				panelMain.validate();
+			}
+		});
 		btnNewButton_1_1_1.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton_1_1_1.setForeground(Color.WHITE);
 		btnNewButton_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnNewButton_1_1_1.setBorder(null);
 		btnNewButton_1_1_1.setBackground(new Color(61, 157, 245));
 		btnNewButton_1_1_1.setBounds(51, 0, 129, 49);
 		panel_3_1_1_1_1_1_1_1_1.add(btnNewButton_1_1_1);
+		
+		JLabel lblUserAccount = new JLabel("");
+		lblUserAccount.setBounds(10, 10, 32, 32);
+		panel_3_1_1_1_1_1_1_1_1.add(lblUserAccount);
+		lblUserAccount.setIcon(new ImageIcon(img_user_account));
 		
 		JPanel panel_3_1_1_1_1_1_1_1_1_1 = new JPanel();
 		panel_3_1_1_1_1_1_1_1_1_1.setLayout(null);
@@ -153,6 +201,14 @@ public class Main extends JFrame {
 		panel_1.add(panel_3_1_1_1_1_1_1_1_1_1);
 		
 		JButton btnNewButton_1_1_1_1 = new JButton("Qu\u1EA3n l\u00FD s\u00E1ch");
+		btnNewButton_1_1_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				children1 = new QuanLiSach();
+				panelMain.removeAll();
+				panelMain.add(children1);
+				panelMain.validate();
+			}
+		});
 		btnNewButton_1_1_1_1.setHorizontalAlignment(SwingConstants.LEFT);
 		btnNewButton_1_1_1_1.setForeground(Color.WHITE);
 		btnNewButton_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
@@ -161,14 +217,27 @@ public class Main extends JFrame {
 		btnNewButton_1_1_1_1.setBounds(51, 0, 129, 49);
 		panel_3_1_1_1_1_1_1_1_1_1.add(btnNewButton_1_1_1_1);
 		
+		JLabel lblQLSach = new JLabel("");
+		lblQLSach.setBounds(10, 10, 32, 32);
+		panel_3_1_1_1_1_1_1_1_1_1.add(lblQLSach);
+		lblQLSach.setIcon(new ImageIcon(img_course));
+		
 		JPanel panel_3_1_1_1_1_1_1_1_1_1_1 = new JPanel();
-		panel_3_1_1_1_1_1_1_1_1_1_1.setForeground(Color.WHITE);
 		panel_3_1_1_1_1_1_1_1_1_1_1.setLayout(null);
+		panel_3_1_1_1_1_1_1_1_1_1_1.setForeground(Color.WHITE);
 		panel_3_1_1_1_1_1_1_1_1_1_1.setBackground(new Color(61, 157, 245));
 		panel_3_1_1_1_1_1_1_1_1_1_1.setBounds(10, 58, 180, 49);
 		panel_1.add(panel_3_1_1_1_1_1_1_1_1_1_1);
 		
 		JButton btnNewButton_1_1_1_1_1 = new JButton("M\u01B0\u1EE3n s\u00E1ch");
+		btnNewButton_1_1_1_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				children1 = new MuonSach();
+				panelMain.removeAll();
+				panelMain.add(children1);
+				panelMain.validate();
+			}
+		});
 		btnNewButton_1_1_1_1_1.setHorizontalAlignment(SwingConstants.LEFT);
 		btnNewButton_1_1_1_1_1.setForeground(Color.WHITE);
 		btnNewButton_1_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
@@ -177,10 +246,10 @@ public class Main extends JFrame {
 		btnNewButton_1_1_1_1_1.setBounds(51, 0, 129, 49);
 		panel_3_1_1_1_1_1_1_1_1_1_1.add(btnNewButton_1_1_1_1_1);
 		
-		JLabel lblIonLogo = new JLabel("");
-		lblIonLogo.setBounds(10, 10, 27, 28);
-		contentPane.add(lblIonLogo);
-		lblIonLogo.setIcon(new ImageIcon(img_logo));
+		JLabel lblBook = new JLabel("");
+		lblBook.setBounds(10, 10, 32, 32);
+		panel_3_1_1_1_1_1_1_1_1_1_1.add(lblBook);
 		setLocationRelativeTo(null);
+		lblBook.setIcon(new ImageIcon(img_book));
 	}
 }
